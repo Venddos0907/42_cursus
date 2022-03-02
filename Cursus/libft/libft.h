920f83f38d4ee50095c79086c28ad27e03289b2d
Venddos0907 <100137446+Venddos0907@users.noreply.github.com>
2022-03-02 15:33:43 +0100

    partie 2

--- Cursus/libft/libft.h
+++ Cursus/libft/libft.h
@@ -6,15 +6,16 @@
 /*   By: mmetzger <mmetzger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:31:52 by mmetzger          #+#    #+#             */
-/*   Updated: 2022/02/28 14:36:31 by mmetzger         ###   ########.fr       */
+/*   Updated: 2022/03/02 15:32:29 by mmetzger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
+
 
 /* PART 1 */
 int		ft_isalpha(int c);
@@ -54,21 +55,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-/* BONUS */
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
